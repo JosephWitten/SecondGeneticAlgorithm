76def6eea65ac1c2f15754da57cb1a67d09b1921
JosephWitten <51753861+JosephWitten@users.noreply.github.com>
2020-04-30 16:55:51 +0100

    Add files via upload

--- Genetic.js
+++ Genetic.js
@@ -1,9 +1,9 @@
 //To do
 // Create population - yum
-// fitness function
-// selection function
-// breed function
-// mutation function
+// fitness function - yum
+// selection function - yum
+// breed function - yum
+// mutation function - yum
 
 const target = "Joseph Witten"
 var population = []
@@ -11,14 +11,42 @@ var fitness = []
 
 
 function start() {
-    population = createPopulation(10);
+    population = createPopulation(100);
+    console.log("INIT POP")
     console.log(population)
+
+    continuous:
+    while(true) {
+
     fitness = getFitness(population);
+    console.log("FITNESS")
     console.log(fitness)
+    matingPool = getMatingPool(fitness)
+    console.log("MATING POOL")
+    console.log(matingPool)
+    population = selectAndBreed(matingPool)
+    console.log("POPULATION")
+    console.log(population)
+    mutate(population, 2)
+
+    
+        
+    if(population.includes(target)){
+        console.log("DONE")
+        indexOfWord = population.indexOf(target);
+        console.log(indexOfWord)
+        console.log(population[indexOfWord])
+ 
+        break continuous;
+    }
+
+
+    
+    }
 }
 
 
-function createPopulation(amount) {
+function createPopulation(amount) {6
     var InitPopulation = []
     for (let index = 0; index < amount; index++) {
         InitPopulation.push(randomString())
@@ -36,6 +64,7 @@ function randomString() {
     return result
 }
 
+
 function getFitness(population) {
     var Initfitness = [];
     for (let i = 0; i < population.length; i++) {
@@ -46,8 +75,67 @@ function getFitness(population) {
                 sum = sum + 1;
             }
         }
-        console.log(sum)
         Initfitness.push(sum)
     }
     return Initfitness
 }
+
+function getMatingPool(fitness) {
+    matingPool = []
+    for (let i = 0; i < population.length; i++) {
+        for (let j = 0; j < fitness[i]; j++) {
+            matingPool.push(population[i])
+        }
+    }
+    return matingPool
+}
+
+function selectAndBreed(matingPool) {
+    if (Math.max(matingPool) == 0) {
+        console.log("EMPTY MATING POOL")
+        matingPool.push(population[Math.floor(Math.random() * population.length)])
+    }
+    newPop = []
+    for (let i = 0; i < population.length; i++) {
+        TempMatingPool = matingPool
+        firstRandomChoice = Math.floor(Math.random() * matingPool.length)
+        secondRandomChoice = Math.floor(Math.random() * TempMatingPool.length)
+        newChromosome = ""
+     
+        for(let j = 0; j < target.length; j++) {
+            var randomBool = Math.random() >= 0.5;
+            if (randomBool) {
+                newChromosome += matingPool[firstRandomChoice].charAt(j);
+            }
+            else {
+                newChromosome += TempMatingPool[secondRandomChoice].charAt(j);
+            }
+        }
+        newPop.push(newChromosome);
+        
+    }
+    return newPop
+}
+
+function mutate(population, rate) {
+
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy ";
+    var charactersLength = characters.length;
+
+    amountToChange = Math.ceil((population.length/100)*rate)
+    for (let i = 0; i < amountToChange; i++) {
+        ChromosomeToChange = Math.floor(Math.random() * population.length)
+        geneToChange = Math.floor(Math.random() * target.length)
+        randomChar = characters.charAt(Math.floor(Math.random() * charactersLength));
+        population[ChromosomeToChange] = replaceCharAt(population[ChromosomeToChange], geneToChange, randomChar)
+        // console.log("Element of population that has been change")
+        // console.log(ChromosomeToChange)
+        // console.log(population[ChromosomeToChange])
+    }
+}
+
+function replaceCharAt(stringToChange, index, replacement) {
+    s = ""
+    s = stringToChange.substring(0, index) + replacement + stringToChange.substring(index + 1, stringToChange.length);
+    return s
+    }
